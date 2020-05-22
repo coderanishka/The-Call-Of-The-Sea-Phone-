447ae5fe68e2ab25f439cecc554d33cb666fdd34
coderanishka <62501236+coderanishka@users.noreply.github.com>
2020-05-22 19:45:26 +0530

    Update sketch2.js

--- The-Call-Of-The-Sea/sketch2.js
+++ The-Call-Of-The-Sea/sketch2.js
@@ -93,7 +93,7 @@ function draw(){
   hook.x = boat.x - 150;
 
   strokeWeight(7);
-  line(hook.x+10,boat.x-500,hook.x+10,hook.y-25);
+  line(hook.x,boat.x,hook.x,hook.y);
 
   if (keyCode === UP_ARROW && hook.y <innerHeight){
     hook.y-=10;
