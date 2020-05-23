e7e65094ba5c2b07d1536964f388e7be0379f42f
coderanishka <62501236+coderanishka@users.noreply.github.com>
2020-05-23 17:42:28 +0530

    Update sketch.js

--- The-Call-Of-The-Sea/sketch.js
+++ The-Call-Of-The-Sea/sketch.js
@@ -23,24 +23,5 @@ function setup(){
 }
 
 function draw(){
-
-    //background("pink");
-    /*fill(0);
-    textSize(30);
-    text("Game1 : SAVING MR.TURTLE",50,250);
-    text("Game2 : TRASH HUNTER",50,500);
-
-    fill(255);
-    textSize(20);
-    text("Are you ready to feed ",270,300);
-    text("Mr.Turtle? But make Sure",270,330);
-    text("you don't let it eat all the",270,360);
-    text("the garbage..Or He'll die..",270,390);
-    
-    textSize(20);
-    text("Can you collect most of ",270,550);
-    text("the garbage just in time",270,580);
-    text("to save the Marine Life?",270,610);*/
-   
-   drawSprites()
+    drawSprites()
 }
